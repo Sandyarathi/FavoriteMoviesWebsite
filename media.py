0c70330699928d2f8a14b7b974eb9bc393bb5b96
Sandyarathi <sandyarathi1212@gmail.com>
2015-08-11 19:08:59 -0700

    Run PEP8 check online

--- media.py
+++ media.py
@@ -1,22 +1,30 @@
-__author__ = 'sandyarathidas'
 import webbrowser
+
+
 class Movie():
     """
-    Defines the movie data structure to store all the properties associated with the movie object
+    __author__ = 'sandyarathidas'
+    Defines the movie data structure to store all
+    the properties associated with the movie object.
     """
-    def __init__(self, movie_title, movie_storyLine, movie_posterImage, movie_trailerlink):
+    def __init__(
+        self,
+        movie_title,
+        movie_storyLine,
+        movie_posterImage,
+        movie_trailerlink):
         """
-        Contructor Instantiates the movie object with the properties set by user.
+        Contructor Instantiates the movie object
+        with the properties set by user.
         """
-        self.title=movie_title
-        self.storyLine=movie_storyLine
-        self.poster_image_url=movie_posterImage
-        self.trailer_youtube_url=movie_trailerlink
+        self.title = movie_title
+        self.storyLine = movie_storyLine
+        self.poster_image_url = movie_posterImage
+        self.trailer_youtube_url = movie_trailerlink
+
     def show_trailer(self):
         """
-        Defines a show_trailer method to display the trailer when clicked on any movie poster
+        Defines a show_trailer method to display
+        the trailer when clicked on any movie poster
         """
         webbrowser.open(self.trailerlink)
-
-
-
