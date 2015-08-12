072b71d557c6af75f89c5ae9d04de6351c90c1be
Sandyarathi <sandyarathi1212@gmail.com>
2015-08-11 18:59:32 -0700

    Run file PEP8 check online

--- entertainment_centre.py
+++ entertainment_centre.py
@@ -1,50 +1,49 @@
-__author__ = 'sandyarathidas'
 import media
 import movie_WebPageLauncher
 """
-Instantiates the movie objects by passing parameter values to the Movie class constructor 
-and then creates a list of all objects instantiated.
+__author__ = 'sandyarathidas'
+Instantiates the movie objects by passing parameter
+values to the Movie class constructor and then
+creates a list of all objects instantiated.
 """
-toy_story=media.Movie(
-  "Toy Story", 
-  "Story of boys and toys", 
-  "http://www.disneymania.com.br/wp-content/uploads/2010/04/toy-story-3-personagens-final-1.jpg", #noqa
-  "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie(
+    "Toy Story",
+    "Story of boys and toys",
+    "http://www.disneymania.com.br/wp-content/uploads/2010/04/toy-story-3-personagens-final-1.jpg",  # noqa
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar=media.Movie(
-  "Avatar",
-  "Scientists and aliens",
-  "http://www.masculinity-movies.com/wp-content/uploads/2010/02/avatar-movie-poster.jpg", #noqa
-  "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+avatar = media.Movie(
+    "Avatar",
+    "Scientists and aliens",
+    "http://www.masculinity-movies.com/wp-content/uploads/2010/02/avatar-movie-poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
-interstellar=media.Movie(
-  "Interstellar",
-  "Space drama",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt6Z71dDSl04F3G1OCff21vRh8i26RVEXfMzK2UchzyJJyzF1F", #noqa
-  "https://www.youtube.com/watch?v=0vxOhd4qlnA")
+interstellar = media.Movie(
+    "Interstellar",
+    "Space drama",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt6Z71dDSl04F3G1OCff21vRh8i26RVEXfMzK2UchzyJJyzF1F",  # noqa
+    "https://www.youtube.com/watch?v=0vxOhd4qlnA")
 
-bahubali=media.Movie(
-  "Bahubali",
-  "Mythological story of a great king",
-  "http://bollywoodflick.in/wp-content/uploads/2015/07/baahubali-movie-posters.jpg",#noqa
-  "https://www.youtube.com/watch?v=sOEg_YZQsTI");
+bahubali = media.Movie(
+    "Bahubali",
+    "Mythological story of a great king",
+    "http://bollywoodflick.in/wp-content/uploads/2015/07/baahubali-movie-posters.jpg",  # noqa
+    "https://www.youtube.com/watch?v=sOEg_YZQsTI")
 
-tangled=media.Movie(
-  "Tangled",
-  "Princess with long hair",
-  "http://i01.i.aliimg.com/wsphoto/v0/32321942623/-font-b-Tangled-b-font-Rapunzel-font-b-Movie-b-font-Fabric-font-b-poster.jpg",#noqa
-  "https://www.youtube.com/watch?v=ip_0CFKTO9E")
+tangled = media.Movie(
+    "Tangled",
+    "Princess with long hair",
+    "http://i01.i.aliimg.com/wsphoto/v0/32321942623/-font-b-Tangled-b-font-Rapunzel-font-b-Movie-b-font-Fabric-font-b-poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=ip_0CFKTO9E")
 
-minions=media.Movie(
-  "Minions",
-  "Minions Mania",
-  "http://www.aroundhawaii.com/wp-content/uploads/2015/07/minions-2015-movie-wallpaper-poster-bob-kevin-stuart-poster-wallpaper-scarlet-overkill-sandra-bullock-despicable-me.jpg", #noqa
-  "https://www.youtube.com/watch?v=dVDk7PXNXB8")
+minions = media.Movie(
+    "Minions",
+    "Minions Mania",
+    "http://www.aroundhawaii.com/wp-content/uploads/2015/07/minions-2015-movie-wallpaper-poster-bob-kevin-stuart-poster-wallpaper-scarlet-overkill-sandra-bullock-despicable-me.jpg",  # noqa
+    "https://www.youtube.com/watch?v=dVDk7PXNXB8")
 
 # define a list of all the movie objects
-movie_list=[toy_story, avatar, interstellar, bahubali, tangled, minions]
+movie_list = [toy_story, avatar, interstellar, bahubali, tangled, minions]
 
-# pass the list of movies to the open_movies_page method in movie_WebPageLauncher.py
+# pass the list of movies to the method in movie_WebPageLauncher.py
 movie_WebPageLauncher.open_movies_page(movie_list)
-
-
